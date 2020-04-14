c6b15687052bacdb78f2902a0b9b2c8fd3c4f5b3
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-04-14 07:01:35 -0500

    create 5 object literals

---
+++ app.js
@@ -0,0 +1,34 @@
+let person1 = {
+    name: 'Simba',
+    sayHello: function() {
+        console.log(`Hello! My name is ${person1.name}.`);
+    }
+};
+
+let person2 = {
+    name: 'Nala',
+    sayHello: function() {
+        console.log(`Hello! My name is ${person2.name}.`);
+    }
+};
+
+let person3 = {
+    name: 'Rafiki',
+    sayHello: function() {
+        console.log(`Hello! My name is ${person3.name}`);
+    }
+};
+
+let person4 = {
+    name: 'Timon',
+    sayHello: function() {
+        console.log(`Hello! My name is ${person4.name}.`);
+    }
+};
+
+let person5 = {
+    name: 'Pumba',
+    sayHello: function() {
+        console.log(`Hello! My name is ${person5.name}`);
+    }
+};
